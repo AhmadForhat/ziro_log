2a01d98ec752fdd30c45fa9be4e285418d46681f
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-06-09 02:17:56 -0300

    envio page navagation

--- src/App/Router.js
+++ src/App/Router.js
@@ -1,12 +1,16 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Router2 as routeMatcher } from '@ziro/router'
-import Form from './Form/index'
 import { Menu } from './Menu/index'
 import NotFound from '@bit/vitorbarbosa19.ziro.not-found'
+import { useRoute, useLocation } from 'wouter'
+import Form from '../App/Form/index'
+import Envio from '../App/Envio/index'
 
 const Router = ({ isLogged }) => {
+    const [match, params] = useRoute('/:envio?')
+    const [location] = useLocation()
     const publicRoutes = {
-        '/': <Menu title='Frete'><Form /></Menu>,
+        [match ? location : null]: <Form {...params} />,
     }
     return routeMatcher(isLogged, publicRoutes, <Form />, <NotFound fallback='/' />)
 }
